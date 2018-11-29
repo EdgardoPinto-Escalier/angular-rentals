043d30f05aa0dbd87f980b0eee0961942d9bab33
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-11-29 21:56:30 +0100

    Created more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA }  from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./includes/header/header.component";
+import { RentalComponent } from './rental/rental.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
+
+const routes: Routes = [
+  {path: '', component: RentalComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HeaderComponent
+    HeaderComponent, RentalComponent, RentalListComponent, RentalListItemComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
