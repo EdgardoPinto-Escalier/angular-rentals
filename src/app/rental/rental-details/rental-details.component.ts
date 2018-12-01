14c8ac3642c11a9129c1b7ee3cf82b66fcd42632
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-12-01 21:27:49 +0100

    Created Rental details component

---
+++ src/app/rental/rental-details/rental-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rentals-rental-details',
+  templateUrl: './rental-details.component.html',
+  styleUrls: ['./rental-details.component.scss']
+})
+export class RentalDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalComponent } from './rental.component';
 
 import { RentalService } from './shared/rental.service';
+import { RentalDetailsComponent } from './rental-details/rental-details.component';
+
+const routes: Routes = [
+  { path: 'rentals', 
+    component: RentalComponent,
+    children: [
+      { path: '', component: RentalListComponent }
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     RentalListComponent, 
     RentalListItemComponent,
-    RentalComponent
+    RentalComponent,
+    RentalDetailsComponent
   ],
   imports: [CommonModule],
   providers: [RentalService]
