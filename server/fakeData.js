4605e97451a768e5d42c55acce6d1719fe8babf5
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-12-03 17:08:35 +0100

    Database has been created

---
+++ server/fakeData.js
@@ -0,0 +1,52 @@
+const Rental = require('./models/rental');
+
+class FakeData {
+  constructor() {
+    this.rentals = [{
+      title: "Nice appartment with ocean views!",
+      city: "Barcelona",
+      street: "Carrer Arago 100",
+      category: "apartment",
+      image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+      bedrooms: 4,
+      shared: true,
+      description: "Very nice apartment in the center of Barcelona city.",
+      dailyRate: 65
+      },
+      {
+      title: "Modern apartment in the center",
+      city: "Madrid",
+      street: "Calle the la Inmaculada 52",
+      category: "apartment",
+      image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+      bedrooms: 1,
+      shared: false,
+      description: "Very nice apartment in center of Madrid",
+      dailyRate: 36
+      },
+      {
+      title: "Old house in the middle of nature!",
+      city: "Valencia",
+      street: "Carrer de L'Anoia 49",
+      category: "house",
+      image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+      bedrooms: 5,
+      shared: true,
+      description: "Very nice house with spanish style close to the mountains.",
+      dailyRate: 85
+    }]
+  }
+
+  pushRentalsToDB() {
+    this.rentals.forEach((rental) => {
+      const newRental = new Rental(rental);
+      newRental.save();
+    })
+  }
+
+  seedTheDb() {
+    this.pushRentalsToDB();
+  }
+}
+
+module.exports = FakeData;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express'); // Here we import express inside node.js
 const mongoose = require('mongoose'); // Import Mongoose
 const config = require('./config/dev'); // Import DB configuration details
+const Rental = require('./models/rental'); // Import the Schema
+const FakeData = require('./fakeData');
 
-mongoose.connect(config.DB_URI, {useNewUrlParser: true});
+
+mongoose.connect(config.DB_URI, {useNewUrlParser: true}).then(() => {
+  fakeData = new FakeData();
+  fakeData.seedTheDb();
+});
 
 const app = express();
 
@@ -13,5 +19,5 @@ app.get('/rentals', function(req, res) {
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function() {
-  console.log('Listening to port 3001');
+  console.log('The Server is running on port 3001, Congratulations!!!');
 });
