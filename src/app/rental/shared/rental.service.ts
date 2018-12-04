43559c722bf3c9f75dbad46614020caca3ccee7c
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2018-12-04 11:09:11 +0100

    Updated rental service to get data from DB

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,82 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Rental } from './rental.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class RentalService {
   
-  private rentals: Rental[] = [{
-    id: "1",
-    title: "Centric apartment well located",
-    city: "Madrid",
-    street: "Calle del arpa 132",
-    category: "Apartment",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 3,
-    description: "Very nice and spacious apartment.",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "30/11/2018"
-  },
-  {
-    id: "2",
-    title: "Amazing new apartment",
-    city: "Paris",
-    street: "Rua Le Roche 441",
-    category: "Apartment",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 3,
-    description: "Great deal, very nice new apartment.",
-    dailyRate: 51,
-    shared: false,
-    createdAt: "27/11/2018"
-  },
-  {
-    id: "3",
-    title: "Small but cozy apartment",
-    city: "Berlin",
-    street: "144 Höffman st",
-    category: "Apartment",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 3,
-    description: "Very nice and spacious apartment.",
-    dailyRate: 30,
-    shared: false,
-    createdAt: "25/11/2018"
-  },
-  {
-    id: "4",
-    title: "Well located apartment in the city",
-    city: "Malmö",
-    street: "Lilla Vägen 144",
-    category: "Apartment",
-    image: "http://via.placeholder.com/350x250",
-    bedrooms: 3,
-    description: "Opportunity nice apartment in the center of Malmö.",
-    dailyRate: 59,
-    shared: false,
-    createdAt: "29/11/2018"
-  }];
+  constructor(private http: HttpClient) {}
 
-  public getRentalById(rentalId: string): Observable<Rental> {
-    return new Observable<Rental>((observer) => {
-      
-      setTimeout(() => {
-        const foundRental = this.rentals.find((rental) => {
-          return rental.id == rentalId;
-        });
-        observer.next(foundRental);
-      }, 500);
-    });
+  public getRentalById(rentalId: string): Observable<any> {
+    return this.http.get('/api/v1/rentals/' + rentalId);
   }
 
-  public getRentals():   Observable<Rental[]> {
-    return new Observable<Rental[]>((observer) => {
-
-      setTimeout(() => {
-        
-        observer.next(this.rentals);
-      }, 1000);     
-    });
+  public getRentals():   Observable<any> {
+  return this.http.get('/api/v1/rentals');
   }
 }
